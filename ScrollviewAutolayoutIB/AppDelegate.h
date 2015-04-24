51513bb4344674682f09265e69005089a646999b
dbarabander <barabander@gmail.com>
2015-04-24 09:30:34 -0400

    initial commit

---
+++ ScrollviewAutolayoutIB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollviewAutolayoutIB
+//
+//  Created by Daniel Barabander on 4/24/15.
+//  Copyright (c) 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollviewAutolayoutIB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollviewAutolayoutIB
+//
+//  Created by Daniel Barabander on 4/24/15.
+//  Copyright (c) 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
